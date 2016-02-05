25c958de0d7b46e8ac8df70486c971ed1fef608a
Zachary Tessler <ztessler@gmail.com>
2016-02-05 14:39:44 -0500

    clip srtm rasters to delta shapes

--- srtm/process_srtm.py
+++ srtm/process_srtm.py
@@ -1,6 +1,7 @@
 import os
 import geopandas
 import rasterio
+from rasterio.features import rasterize
 from affine import Affine
 import cartopy.crs as ccrs
 from cartopy.io.srtm import SRTM1Source, SRTM3Source, SRTMDownloader
@@ -39,3 +40,33 @@ def delta_srtm_composite(env, target, source):
         dst.write(image, 1)
 
     return 0
+
+def clip_srtm_to_delta(env, target, source):
+    dname = env['delta']
+    resolution=env['resolution']
+    deltas = geopandas.GeoDataFrame.from_file(str(source[0])).set_index('Delta')
+    delta = deltas['geometry'][dname]
+
+    nodata = -9999
+
+    with rasterio.open(str(source[1]), 'r') as src:
+        kwargs = src.meta.copy()
+        del kwargs['transform']
+
+        mask = rasterize(delta, out_shape=src.shape, transform=src.affine, dtype=src.dtypes[0])
+        window = rasterio.get_data_window(mask, 0)
+        image = src.read(1, window=window)
+        mask = mask[slice(*window[0]), slice(*window[1])]
+        image[mask==0] = nodata
+
+        kwargs.update({
+            'height': window[0][1] - window[0][0],
+            'width': window[1][1] - window[1][0],
+            'affine': src.window_transform(window),
+            'nodata': nodata})
+
+        with rasterio.open(str(target[0]), 'w', **kwargs) as dst:
+            dst.write(image, 1)
+
+        return 0
+
