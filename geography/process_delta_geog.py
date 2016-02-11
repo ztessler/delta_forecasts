1348a7b9d91552a23b95073af536c22a70fc80ed
Zachary Tessler <ztessler@gmail.com>
2016-02-11 17:28:15 -0500

    get all basins that lead into each delta

--- geography/process_delta_geog.py
+++ geography/process_delta_geog.py
@@ -1,12 +1,13 @@
-import os
-import fiona
+import numpy as np
+import json
 import geopandas
 import shapely.geometry as sgeom
+from rasterstats import zonal_stats
+from collections import OrderedDict
 
 def group_delta_shps(env, target, source):
     shpfile = str(source[0])
 
-    # with fiona.open(shpfile) as shp:
     deltas = geopandas.GeoDataFrame.from_file(shpfile)
     crs = deltas.crs
 
@@ -30,5 +31,24 @@ def delta_geojson(env, target, source):
 
     return 0
 
+def contributing_basins(env, target, source):
+    def ma_unique_values(ma):
+        return {int(b) for b in ma[np.logical_not(ma.mask)]}
+
+    stats = zonal_stats(
+            str(source[0]), str(source[1]),
+            geojson_out=True,
+            add_stats={'basins': ma_unique_values})
+
+    data = OrderedDict()
+    for s in stats:
+        deltainfo = OrderedDict()
+        deltainfo['DeltaID'] = int(s['properties']['DeltaID'])
+        deltainfo['basin_ids'] = sorted(s['properties']['basins'])
+        data[s['properties']['Delta']] = deltainfo
+
+    with open(str(target[0]), 'w') as outfile:
+        json.dump(data, outfile)
+    return 0
 
 
