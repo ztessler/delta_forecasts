8c0f881c5e53957d8938b04ca5d429b565e7b889
Zachary Tessler <ztessler@gmail.com>
2016-02-17 14:45:00 -0500

    regrid rgis tif files to 06min if needed

--- rgis/process_rgis.py
+++ rgis/process_rgis.py
@@ -1,7 +1,42 @@
 import numpy as np
 from netCDF4 import Dataset
 import rasterio
+from rasterio.warp import reproject, RESAMPLING
 from affine import Affine
+
+
+def regrid_to_06min(env, target, source):
+    dst_dx = 0.1
+    dst_dy = -0.1
+    dst_width = 3600
+    dst_height = 1800
+    dst_affine = (Affine.scale(dst_dx, dst_dy) *
+                  Affine.translation(-dst_width/2., -dst_height/2.))
+    data06 = np.zeros((dst_height, dst_width))
+
+    with rasterio.open(str(source[0]), 'r') as src:
+        reproject(src.read(1),
+                  data06,
+                  src_transform=src.affine,
+                  src_crs=src.crs,
+                  src_nodata=src.nodata,
+                  dst_transform=dst_affine,
+                  dst_crs=src.crs,
+                  dst_nodata=src.nodata,
+                  resampling=RESAMPLING.bilinear)
+        kwargs = src.meta
+
+    kwargs.update({
+	    'transform': dst_affine,
+	    'affine': dst_affine,
+	    'width': dst_width,
+	    'height': dst_height,
+	})
+
+    with rasterio.open(str(target[0]), 'w', **kwargs) as dst:
+        dst.write(data06, 1)
+    return 0
+
 
 def georef_nc(env, target, source):
     nc = Dataset(str(source[0]))
