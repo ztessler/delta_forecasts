e248ff46a12628cf6d459882bd8fdae0a4136e18
Zachary Tessler <ztessler@gmail.com>
2016-02-17 12:21:15 -0500

    compute pixel areas of a given raster. raster must be in platecarree, code assumes constant pixel area across rows

--- geography/process_delta_geog.py
+++ geography/process_delta_geog.py
@@ -1,9 +1,12 @@
 import numpy as np
 import json
 import geopandas
+import rasterio
 import shapely.geometry as sgeom
+import cartopy.crs as ccrs
 from rasterstats import zonal_stats
 from collections import OrderedDict
+
 
 def group_delta_shps(env, target, source):
     shpfile = str(source[0])
@@ -23,6 +26,7 @@ def group_delta_shps(env, target, source):
     deltas.to_file(str(target[0]), 'ESRI Shapefile')
     return 0
 
+
 def delta_geojson(env, target, source):
     dname = env['delta']
     deltas = geopandas.GeoDataFrame.from_file(str(source[0])).set_index('Delta')
@@ -30,6 +34,7 @@ def delta_geojson(env, target, source):
     delta.reset_index().to_file(str(target[0]), 'GeoJSON')
 
     return 0
+
 
 def contributing_basins(env, target, source):
     def ma_unique_values(ma):
@@ -52,3 +57,35 @@ def contributing_basins(env, target, source):
     return 0
 
 
+def raster_pixel_areas(env, target, source):
+    with rasterio.open(str(source[0]), 'r') as rast:
+        shape = rast.shape
+        rastcrs = rast.crs
+        affine = rast.affine
+        kwargs = rast.meta
+    del kwargs['transform']
+    areas = np.zeros(shape, dtype=np.float)
+
+    if rastcrs['init'] == 'epsg:4326':
+        geod = ccrs.PlateCarree().as_geodetic()
+    else:
+        raise NotImplementedError, 'Only works with lat/lon grid, assumes pixels rows have constant area'
+
+    lon0, lat0 = affine * (0,0)
+    lon1, lat1 = affine * shape[::-1]
+    dlon_2 = affine.a / 2.
+    dlat = affine.e
+    dlat_2 = dlat / 2.
+    lats = np.arange(lat0, lat1, dlat)
+    for j, lat in enumerate(lats):
+        lat_next = lat + dlat
+        laea = ccrs.LambertAzimuthalEqualArea(central_longitude=0,
+                                              central_latitude=lat+dlat_2)
+        poly = sgeom.Polygon(
+                laea.transform_points(geod,
+                                      np.array([-dlon_2, -dlon_2, dlon_2, dlon_2]),
+                                      np.array([lat, lat_next, lat_next, lat])))
+        areas[j,:] = poly.area / 1e6
+
+    with rasterio.open(str(target[0]), 'w', **kwargs) as dst:
+        dst.write(areas, 1)
