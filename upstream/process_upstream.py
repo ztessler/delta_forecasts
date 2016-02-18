f2204afbd1155e3a3e57cd6ed25765079d027ec2
Zachary Tessler <ztessler@gmail.com>
2016-02-18 12:54:14 -0500

    compute max elevation in basins, convert to km

--- upstream/process_upstream.py
+++ upstream/process_upstream.py
@@ -23,8 +23,10 @@ def agg_over_basins(env, target, source):
         aggregate = lambda data, _: np.mean(data)
     elif method == 'weightedmean':
         aggregate = lambda data, weights: np.sum(data * weights) / np.sum(weights)
+    elif method == 'max':
+        aggregate = lambda data, _: np.max(data)
     else:
-        raise ValueError, 'method must be sum, weightedsum, mean, weightedmean'
+        raise ValueError, 'method must be sum, weightedsum, mean, weightedmean, max'
 
     with rasterio.open(str(source[0]), 'r') as basins_rast:
         basins = basins_rast.read(1)
@@ -47,4 +49,10 @@ def agg_over_basins(env, target, source):
         aggregated[delta] = aggregate(data[pixels], weights[pixels])
 
     pandas.Series(aggregated).to_pickle(str(target[0]))
+    return 0
+
+
+def convert_m_to_km(env, target, source):
+    p = pandas.read_pickle(str(source[0]))
+    (p / 1e3).to_pickle(str(target[0]))
     return 0
