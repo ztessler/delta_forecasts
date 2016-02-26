dcf6cda743d636e7ac12f51c6ca3c93e712becf7
Zachary Tessler <ztessler@gmail.com>
2016-02-26 12:47:14 -0500

    move zonal stats computation out of population module into gis

--- gis.py
+++ gis.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas
+import geopandas
 import rasterio
-from rasterio.warp import RESAMPLING, reproject
+from rasterio.warp import RESAMPLING, reproject, calculate_default_transform
 from affine import Affine
 from netCDF4 import Dataset
 import cartopy.crs as ccrs
 import shapely.geometry as sgeom
+from rasterstats import zonal_stats
 
 
 def regrid_to_06min(env, target, source):
@@ -127,4 +130,54 @@ def raster_pixel_areas(env, target, source):
 
     with rasterio.open(str(target[0]), 'w', **kwargs) as dst:
         dst.write(areas, 1)
+    return 0
+
+
+def delta_zonal_stats(env, target, source):
+    # reprojects delta shape to LamberAzimuthalEqualArea projection
+    # centered on delta.  Extracts data from (global) raster, warps to same
+    # projection and calculates zonal stats within delta
+    deltas = geopandas.GeoDataFrame.from_file(str(source[0])).set_index('Delta')
+    raster = rasterio.open(str(source[1]))
+    nodata = raster.nodata
+    minlon, minlat, maxlon, maxlat = raster.bounds
+
+    centroids = deltas.centroid
+    bounds = deltas.bounds
+    stats = {}
+    for dname in deltas.index:
+        # delta location
+        lon, lat = np.array(centroids.loc[dname])
+        d_minlon, d_minlat, d_maxlon, d_maxlat = bounds.loc[dname]
+
+        # reproject delta shape
+        laea = ccrs.LambertAzimuthalEqualArea(central_longitude=lon,
+                                              central_latitude=lat)
+        delta = deltas.loc[[dname]].to_crs(laea.proj4_params)['geometry']
+
+        # extract minimum raster around delta
+        d_maxy, d_minx = raster.index(d_minlon, d_minlat)
+        d_miny, d_maxx = raster.index(d_maxlon, d_maxlat)
+        window = ((d_miny, d_maxy+1), (d_minx, d_maxx+1))
+        src = raster.read(1, window=window)
+
+        # get size, bounds, affine of raster subset
+        src_height, src_width = src.shape
+        src_bounds = raster.window_bounds(window)
+        src_affine = raster.window_transform(window)
+
+        # reproject raster
+        dst_affine, dst_width, dst_height = calculate_default_transform(
+                raster.crs, laea.proj4_params, src_width, src_height,
+                *src_bounds)
+        dst = np.ones((dst_height, dst_width)) * nodata
+        reproject(src, dst, src_affine, raster.crs, nodata, dst_affine,
+                laea.proj4_params, nodata, RESAMPLING.bilinear)
+
+        # calculate zonal stats
+        _stats = zonal_stats(delta, dst, affine=dst_affine, nodata=nodata)
+        _stats = _stats[0]   # delta only has a single Multipolygon feature
+        stats[dname] = _stats
+    raster.close()
+    pandas.DataFrame.from_dict(stats, orient='index').to_pickle(str(target[0]))
     return 0
--- population/process_population.py
+++ population/process_population.py
@@ -9,60 +9,6 @@ import rasterio
 from rasterio.features import rasterize
 from rasterio.warp import calculate_default_transform, reproject, RESAMPLING
 import cartopy.crs as ccrs
-from rasterstats import zonal_stats
-
-def delta_population_stats(env, target, source):
-    # reprojects delta shape to LamberAzimuthalEqualArea projection
-    # centered on delta.  Extracts data from (global) raster, warps to same
-    # projection and calculates zonal stats within delta
-    year = env['year']
-    deltas = geopandas.GeoDataFrame.from_file(str(source[0])).set_index('Delta')
-    raster = rasterio.open(str(source[1]))
-    nodata = raster.nodata
-    minlon, minlat, maxlon, maxlat = raster.bounds
-
-    centroids = deltas.centroid
-    bounds = deltas.bounds
-    stats = {}
-    for dname in deltas.index:
-        # delta location
-        lon, lat = np.array(centroids.loc[dname])
-        d_minlon, d_minlat, d_maxlon, d_maxlat = bounds.loc[dname]
-
-        # reproject delta shape
-        laea = ccrs.LambertAzimuthalEqualArea(central_longitude=lon,
-                                              central_latitude=lat)
-        delta = deltas.loc[[dname]].to_crs(laea.proj4_params)['geometry']
-
-        # extract minimum raster around delta
-        d_maxy, d_minx = raster.index(d_minlon, d_minlat)
-        d_miny, d_maxx = raster.index(d_maxlon, d_maxlat)
-        window = ((d_miny, d_maxy+1), (d_minx, d_maxx+1))
-        src = raster.read(1, window=window)
-
-        # get size, bounds, affine of raster subset
-        src_height, src_width = src.shape
-        src_bounds = raster.window_bounds(window)
-        src_affine = raster.window_transform(window)
-
-        # reproject raster
-        dst_affine, dst_width, dst_height = calculate_default_transform(
-                raster.crs, laea.proj4_params, src_width, src_height,
-                *src_bounds)
-        dst = np.ones((dst_height, dst_width)) * nodata
-        reproject(src, dst, src_affine, raster.crs, nodata, dst_affine,
-                laea.proj4_params, nodata, RESAMPLING.bilinear)
-
-        # calculate zonal stats
-        _stats = zonal_stats(delta, dst, affine=dst_affine, nodata=nodata)
-        _stats = _stats[0]   # delta only has a single Multipolygon feature
-        stats[dname] = {
-                'pop_density': _stats['mean'],  # people/sq.km.
-                'pop_count':  _stats['mean'] * delta.area.iloc[0] / 1e6,  # people
-                }
-    raster.close()
-    pandas.DataFrame.from_dict(stats, orient='index').to_pickle(str(target[0]))
-    return 0
 
 
 def clip_pop_to_delta(env, target, source):
