83aac26408b81c7dfd3d1c56e82e217280aea2de
Zachary Tessler <ztessler@gmail.com>
2016-03-10 16:57:58 -0500

    starting surge shapefile is full of errors, lots of data points where rarer storms are listed as smaller. clean up data to ensure return curves are increasing in size

--- hazards/process_hazards.py
+++ hazards/process_hazards.py
@@ -7,11 +7,11 @@ import cartopy.crs as ccrs
 
 def storm_surge_agg_points(env, target, source):
     deltas = geopandas.read_file(str(source[0])).set_index('Delta')
-    surges = geopandas.read_file(str(source[1]))
+    allsurges = geopandas.read_file(str(source[1]))
 
     origcols = ['T_10', 'T_25', 'T_50', 'T_100', 'T_250']
     cols = map(lambda s: float(s[2:]), origcols)
-    surges = surges[['geometry'] + origcols].rename_axis(
+    allsurges = allsurges[['geometry'] + origcols].rename_axis(
             {orig:new for orig, new in zip(['geometry']+origcols, ['geometry']+cols)},
             axis=1)
 
@@ -26,10 +26,38 @@ def storm_surge_agg_points(env, target, source):
         # buffer around convex hull (very slow over all points)
         delta_buff = delta.convex_hull.buffer(25 * 1000) # 25km
         # project back to match surge data
-        poly = delta_buff.to_crs(surges.crs).item()
+        poly = delta_buff.to_crs(allsurges.crs).item()
 
-        delta_surges = surges[surges.within(poly)][cols]
-        mean_surge.loc[dname,:] = delta_surges[delta_surges>0].mean(axis=0)
+        surges = allsurges[allsurges.within(poly)][cols]
+
+        # 0 seems to be used as a flag for missing data
+        # very small surges also in data, remove these
+        # negative surge also clearly wrong
+        surges[surges <= 0.01] = np.nan
+        # remove where there is no or negative increase between levels. larger surges are by defitition rarer
+        surges[surges.diff(axis=1) <= 0] = np.nan
+        # remove any values at higher levels from bad values
+        for i, col in enumerate(surges):
+            for j in np.where(surges[col].isnull())[0]:
+                for ii in range(i+1, len(cols)):
+                    surges.iloc[j, ii] = np.nan
+
+        # surges = surges.dropna(axis=0, how='any')
+        # for i, level in enumerate(surges):
+            # surges[surges[level].isnull()].iloc[:, i:] = np.nan
+
+        # now all cleaned, each row has sane values
+        # starts with positive value, increases from there
+        # can't take mean across all existing data, mean can end up with decreasing values
+        # instead, take mean over rows that have the most available data
+        dropcols = surges.isnull().sum(axis=1).min()
+        if dropcols > 0:
+            surges = surges[cols[:-dropcols]]
+        surges = surges.dropna(how='any', axis=0)
+
+        agg_surge = surges.mean(axis=0)
+        assert (agg_surge.diff().dropna()>0).all(), 'Processing should enforce increasing surge at longer return periods, went wrong'
+        mean_surge.loc[dname,:] = agg_surge
 
     mean_surge.to_pickle(str(target[0]))
     return 0
