2b676177481c711c4006cd7e98afdb810cc7faa4
Zachary Tessler <ztessler@gmail.com>
2016-05-10 16:59:08 -0400

    compute expected value of surge exposure in one year given the surge return values and population elevations, considering pop growth and rslr

--- risk/process_risk.py
+++ risk/process_risk.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import pandas
 
 
@@ -21,3 +22,27 @@ def import_idi_vuln(env, target, source):
     vuln.to_pickle(str(target[0]))
     return 0
 
+
+def surge_expected_expo(env, target, source):
+    '''
+    Compute expected flooding due to surge for any given year.   Adjusts to avoid double counting
+    Estimated as:
+        (prob of 25 year flood) * (exposure to 25 year flood) +
+        (prob of 10 year flood - prob of 25 year flood) * (exposure to 10 year flood) +
+        ...
+    '''
+    exposure = pandas.read_pickle(str(source[0]))
+
+    def one_yr_expected(s):
+        total_exp = 0.0
+        prob_more_extreme = 0.0
+        for ret_period, ret_level in s.sort_index(ascending=False).iteritems():
+            if ret_level is np.nan:
+                continue
+            prob = 1./ret_period
+            total_exp += (prob - prob_more_extreme) * ret_level
+            prob_more_extreme += prob
+        return total_exp
+
+    exposure.apply(one_yr_expected, axis=0).to_pickle(str(target[0]))
+    return 0
